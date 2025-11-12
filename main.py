4cc4c9c846039efa56de1e279034351f201ba7bf
Ozmanbir <159967099+Ozmanbir@users.noreply.github.com>
2025-11-13 02:27:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from flask import Flask
-import requests
+from flask import Flask, redirect
 import os
-
 app = Flask(__name__)
-
-PLAYER_PAGE = "https://catcast.tv/tv-zle"
-
 @app.route('/')
 def index():
-    return f"Token servisi aktif ✅<br>Sayfa: <a href='{PLAYER_PAGE}'>{PLAYER_PAGE}</a>"
-
+    return redirect("https://catcast.tv/tv-zle", code=302)
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
