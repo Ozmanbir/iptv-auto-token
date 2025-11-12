77ec9daa654db9bcc9c1ec10f68711204691ebff
Ozmanbir <159967099+Ozmanbir@users.noreply.github.com>
2025-11-13 02:32:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, redirect
+from flask import Flask,redirect
 import os
-app = Flask(__name__)
+app=Flask(__name__)
 @app.route('/')
 def index():
-    return redirect("https://catcast.tv/tv-zle", code=302)
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+ return redirect("https://catcast.tv/tv-zle",code=302)
+if __name__=='__main__':
+ port=int(os.environ.get('PORT',5000))
+ app.run(host='0.0.0.0',port=port)
