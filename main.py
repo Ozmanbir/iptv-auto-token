d6291c201df1f51b99896da6a750f64a85f95fd9
Ozmanbir <159967099+Ozmanbir@users.noreply.github.com>
2025-11-13 09:05:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect
 import os
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -12,6 +12,6 @@ def live():
     token_url = "https://s.catcast.tv/content/49918/index.m3u8?token=b13e2ae89c49fb4132e0622f19419604"
     return redirect(token_url, code=302)
 
-if name == 'main':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
