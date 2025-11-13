4215543b907a932ab4ce1b9d3b35e64e674931e1
Ozmanbir <159967099+Ozmanbir@users.noreply.github.com>
2025-11-13 07:08:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask, redirect
 import os
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-return "✅ IPTV yönlendirme aktif!<br>Oynatıcı linki: <a href='/live'>/live</a>"
+    return "✅ IPTV yönlendirme aktif!<br>Oynatıcı linki: <a href='/live'>/live</a>"
 
 @app.route('/live')
 def live():
-token_url = "https://s.catcast.tv/content/49918/index.m3u8?token=b13e2ae89c49fb4132e0622f19419604"
-return redirect(token_url, code=302)
+    token_url = "https://s.catcast.tv/content/49918/index.m3u8?token=b13e2ae89c49fb4132e0622f19419604"
+    return redirect(token_url, code=302)
 
-if name == 'main':
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
