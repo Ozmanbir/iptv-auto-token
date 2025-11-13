b0d952573812756959902ae8884e361d63e3d68e
Ozmanbir <159967099+Ozmanbir@users.noreply.github.com>
2025-11-13 06:11:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return """
-    ✅ IPTV yönlendirme aktif!<br>
-    <a href='/live'>📺 Yayın 1</a><br>
-    <a href='/live2'>📺 Yayın 2</a>
+    ✅ IPTV yönlendirme aktif!<br><br>
+    🔹 <a href='/live'>/live</a><br>
+    🔹 <a href='/live2'>/live2</a>
     """
 
+# 1. yayın
 @app.route('/live')
 def live():
     token_url = "https://s.catcast.tv/content/49918/index.m3u8?token=b13e2ae89c49fb4132e0622f19419604"
     return redirect(token_url, code=302)
 
+# 2. yayın
 @app.route('/live2')
 def live2():
     token_url = "https://dc34a9483e0f144e9e.pages.dev/cefc8a875b06cc6da16ca7dd99157dee/DmodYM9CdSNPUZyc2x7VWd/5062/dc34a9483e0f144e9e.pages.dev/chunklist_hd.m3u8?verify=1763016750936~213.238.187.84~bd14cdaa057f57353a74ee68d0a7a394"
